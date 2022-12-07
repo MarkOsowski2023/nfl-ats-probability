902e1ddd9e28a47d285856d5f253a90fa9358bd2
Mark <markosowski2023@u.northwestern.edu>
2022-12-06 19:44:39 -0600

    Rscript

--- R_plots/r_plots.R
+++ R_plots/r_plots.R
@@ -142,6 +142,45 @@ p1 <- game_data |>
     axis.text.y = element_nfl_logo(size = 0.85)
   )
 
+# team win percentage against key spreads
+
+p4 <- game_data |> 
+  group_by(team) |> 
+  mutate(avg_epa = mean(epa)) |> 
+  filter(spread_line %in% key_spread_numbers) |> 
+  group_by(team) |> 
+  mutate(key_games = n()) |> 
+  filter(ats_win == "win") |> 
+  group_by(team) |> 
+  mutate(key_wins = n()) |> 
+  group_by(team) |> 
+  summarise(key_games = mean(key_games),
+            key_wins = mean(key_wins),
+            avg_epa = mean(avg_epa),
+            key_win_percentage = key_wins / key_games) |> 
+  filter(key_games > 50) |> 
+  ggplot(aes(y = reorder(team, key_win_percentage), x = key_win_percentage)) +
+  geom_col(aes(color = team, fill = team), width = 0.7) +
+  nflplotR::scale_color_nfl(type = "secondary") +
+  nflplotR::scale_fill_nfl(alpha = 0.4) +
+  labs(
+    x = "Win % (ATS)",
+    y = "Team", 
+    caption = "DATA: @nflreadR",
+    title = "Team Cover Percentage Against the Spread",
+    subtitle = "Seasons: 2001 - 2022 | Spread: 2.5, 3.0, 3.5, 6.5, 7.0, 7.5 | Minimum 50 Games"
+  ) +
+  theme_fivethirtyeight() +
+  theme(
+    plot.title = element_text(face = "bold", family = "mono"),
+    plot.subtitle = element_text(family = "mono", size = 11),
+    plot.title.position = "plot",
+    axis.title.x = element_text(face = "bold", family = "mono", size = 15, margin = margin(r = 20)),
+    axis.title.y = element_text(face = "bold", family = "mono", size = 15, margin = margin(r = 20)),
+    axis.text.y = element_nfl_logo(size = 0.85)
+  )
+
+p4
 # epa and wins in key spreads
 
 p2 <- game_data |> 
@@ -234,3 +273,4 @@ ggsave("wins_against_the_spread.png", plot = p1)
 ggsave("win_percentage_ats_and_epa.png", plot = p2)
 ggsave("win_percentage_ats_and_def_epa.png", plot = p3)
 ggsave("tom_brady_off_epa.png", plot = tb1)
+ggsave("team_win_percentage_ats.png", plot = p4)
